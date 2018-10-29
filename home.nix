ab169558937c2161b054767cb2a109a1d220d1bb
Matthew Doty <matthew.wampler.doty@gmail.com>
2018-10-29 08:14:52 -0400

    home.nix: Adding shtfmt; leaving note to make custom derivation to bump version

--- home.nix
+++ home.nix
@@ -1,18 +1,27 @@
 { pkgs, ... }:
 let
+
+  haskellPackages = pkgs.haskell.packages.ghc843;
+
+  # Custom Haskell packages
   hiePkgs = import (pkgs.fetchFromGitHub {
     owner = "domenkozar";
     repo = "hie-nix";
     rev = "96af698f0cfefdb4c3375fc199374856b88978dc";
     sha256 = "1ar0h12ysh9wnkgnvhz891lvis6x9s8w3shaakfdkamxvji868qa";
   }) {};
+
   snack = import (pkgs.fetchFromGitHub {
     owner = "nmattia";
     repo = "snack";
     rev = "6dc8caaec88f9a66ad4f66f396db5d5e58d91064";
     sha256 = "0w6gliidknnyv6jlk8533lvji4v6icsq6y1zywfbj9q1xsqw3mx2";
   });
-  haskellPackages = pkgs.haskell.packages.ghc843;
+
+  # TODO: Make custom derivation for latest shfmt
+  # - https://github.com/mvdan/sh/tree/v2.5.1
+  # - https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/text/shfmt/default.nix
+
 in {
   home.packages = [
     # Development
@@ -25,6 +34,7 @@ in {
     pkgs.jq
     pkgs.bat
     pkgs.screen
+    pkgs.shfmt
 
     # Reading
     pkgs.calibre
